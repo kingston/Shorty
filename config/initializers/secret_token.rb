9ad8e6028c0a5a12d29d2c451910fa32fb62f0b3
Kingston <git@kingstontam.com>
2011-04-27 23:14:54 -0700

    Initial commit of Rails framework

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Source::Application.config.secret_token = '53de522da03d0260817e644cdf625214e020ea692dccb2ae1ab14739e3ddfdd8efdb8dc2922b49d65dd6ed55da46eb4f8e640f64830b0af03f7aa1eb05eb2b01'
