c8671c45e83e27cca2a0516a19d0474a9cb5d32d
Kingston <git@kingstontam.com>
2011-05-11 20:37:00 -0700

    Restyled frontend to look more pretty ;)

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$( function() {
+	// Add default label to textboxes
+	var blurFunc = function() {
+		var elem = $(this);
+		if (elem.val() === "") {
+			elem.val(elem.attr("title"));
+			elem.addClass("title-text");
+		}
+	};
+	$(".default-label").focus( function() {
+		var elem = $(this);
+		if (elem.hasClass("title-text")) {
+			elem.removeClass("title-text");
+			elem.val("");
+		}
+	})
+	.blur(blurFunc)
+	.attr('autocomplete', 'off')
+	.each(blurFunc);
+});
