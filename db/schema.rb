2ddb1048cd099fcd8ef1e7f1463e4881d3048010
kingston <web@kingstontam.com>
2012-06-24 14:00:07 -0700

    Replace Compass with Bourbon and updated rest to Rails 3.2.5

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -15,8 +16,8 @@ ActiveRecord::Schema.define(:version => 20110519222519) do
   create_table "url_entries", :force => true do |t|
     t.string   "key"
     t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "attachment"
   end
 
