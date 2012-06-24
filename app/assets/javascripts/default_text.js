d92f537caac82cd0cd29b28c1c6bded428f6765e
kingston <web@kingstontam.com>
2012-06-24 12:09:59 -0700

    Moving to Sprockets system

--- app/assets/javascripts/default_text.js
+++ app/assets/javascripts/default_text.js
@@ -1,5 +1,4 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+// Default text switching-out
 
 $( function() {
 	// Add default label to textboxes
